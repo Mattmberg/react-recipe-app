f602598400aff23b99e16963116e4d5b2469237a
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2024-01-18 21:58:02 -0500

    Set up initial data structure and dummy data

---
+++ react-recipe-app/src/data.js
@@ -0,0 +1,202 @@
+export const recipeData = [
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+    {
+        id: 1,
+        name:"",
+        ingredients: ["", ""],
+    },
+]

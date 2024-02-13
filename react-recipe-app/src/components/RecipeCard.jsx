64a79b96f7e3b1c752200b982dc1eeb0c324be69
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2024-02-12 22:11:48 -0500

    Basic structure done

--- react-recipe-app/src/components/RecipeCard.jsx
+++ react-recipe-app/src/components/RecipeCard.jsx
@@ -4,7 +4,7 @@ export const RecipeCard = ({ id, name, description, ingredients}) => {
     return (
         <div className ="">
             <div className="">
-                <div><input type="checkbox" value={name} id={name}/><label className=""><b>{name}</b></label></div>
+                <div><label className=""><b>{name}</b></label></div>
                 <p className="">
                     {description}
                 </p>

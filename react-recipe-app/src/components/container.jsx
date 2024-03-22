f39fdef141112f019fcbdf03dd803e57cca9396e
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2024-03-21 22:35:38 -0400

    Cleaned up code for each day.

--- react-recipe-app/src/components/container.jsx
+++ react-recipe-app/src/components/container.jsx
@@ -7,7 +7,6 @@ import { useState } from "react";
 import { send } from 'emailjs-com';
 
 export const Container = () => {
-
     const [meals, setMeals] = useState({
         sunMeal: "",
         monMeal: "",
@@ -96,9 +95,10 @@ export const Container = () => {
                                 <select
                                 name="sunMeal" 
                                 value={meals.sunMeal}                          
-                                onChange={e => { setMeals({
-                                    ...meals, sunMeal: e.target.value
-                                }); setToSend({...toSend, sunMeal: e.target.value });}}>      
+                                onChange={e => { 
+                                    setMeals({ ...meals, sunMeal: e.target.value }); 
+                                    setToSend({...toSend, sunMeal: e.target.value });
+                                }}>      
                                     {recipeData.map(recipe => {
                                         return <option key={recipe.id} value={recipe.name}>{recipe.name}</option>;
                                     })}
@@ -108,9 +108,10 @@ export const Container = () => {
                                 <select
                                 name="monMeal" 
                                 value={meals.monMeal}                          
-                                onChange={e => { setMeals({
-                                    ...meals, monMeal: e.target.value
-                                }); setToSend({...toSend, monMeal: e.target.value });}}>     
+                                onChange={e => { 
+                                    setMeals({ ...meals, monMeal: e.target.value }); 
+                                    setToSend({...toSend, monMeal: e.target.value });
+                                }}>     
                                     {recipeData.map(recipe => {
                                         return <option key={recipe.id} value={recipe.name}>{recipe.name}</option>;
                                     })}
@@ -120,9 +121,10 @@ export const Container = () => {
                                 <select 
                                 name="tuesMeal"
                                 value={meals.tuesMeal}                          
-                                onChange={e => { setMeals({
-                                    ...meals, tuesMeal: e.target.value
-                                }); setToSend({...toSend, tuesMeal: e.target.value });}}>   
+                                onChange={e => { 
+                                    setMeals({ ...meals, tuesMeal: e.target.value }); 
+                                    setToSend({...toSend, tuesMeal: e.target.value });
+                                }}>   
                                     {recipeData.map(recipe => {
                                         return <option key={recipe.id} value={recipe.name}>{recipe.name}</option>;
                                     })}
@@ -132,7 +134,10 @@ export const Container = () => {
                                 <select 
                                 name="wedMeal"
                                 value={meals.wedMeal}                          
-                                onChange={e => { setMeals({ ...meals, wedMeal: e.target.value }); setToSend({...toSend, wedMeal: e.target.value });}}>   
+                                onChange={e => { 
+                                    setMeals({ ...meals, wedMeal: e.target.value }); 
+                                    setToSend({...toSend, wedMeal: e.target.value });
+                                }}>   
                                     {recipeData.map(recipe => {
                                         return <option key={recipe.id} value={recipe.name}>{recipe.name}</option>;
                                     })}
@@ -142,9 +147,10 @@ export const Container = () => {
                                 <select 
                                 name="thurMeal"
                                 value={meals.thurMeal}                          
-                                onChange={e => { setMeals({
-                                    ...meals, thurMeal: e.target.value
-                                }); setToSend({...toSend, thurMeal: e.target.value });}}>   
+                                onChange={e => { 
+                                    setMeals({ ...meals, thurMeal: e.target.value }); 
+                                    setToSend({...toSend, thurMeal: e.target.value });
+                                }}>   
                                     {recipeData.map(recipe => {
                                         return <option key={recipe.id} value={recipe.name}>{recipe.name}</option>;
                                     })}
@@ -154,9 +160,10 @@ export const Container = () => {
                                 <select 
                                 name="friMeal"
                                 value={meals.friMeal}                          
-                                onChange={e => { setMeals({
-                                    ...meals, friMeal: e.target.value
-                                }); setToSend({...toSend, friMeal: e.target.value });}}>   
+                                onChange={e => { 
+                                    setMeals({ ...meals, friMeal: e.target.value }); 
+                                    setToSend({...toSend, friMeal: e.target.value });
+                                }}>   
                                     {recipeData.map(recipe => {
                                         return <option key={recipe.id} value={recipe.name}>{recipe.name}</option>;
                                     })}
@@ -166,9 +173,10 @@ export const Container = () => {
                                 <select 
                                 name="satMeal"
                                 value={meals.satMeal}                          
-                                onChange={e => { setMeals({
-                                    ...meals, satMeal: e.target.value
-                                }); setToSend({...toSend, satMeal: e.target.value });}}>   
+                                onChange={e => { 
+                                    setMeals({ ...meals, satMeal: e.target.value }); 
+                                    setToSend({...toSend, satMeal: e.target.value });
+                                }}>   
                                     {recipeData.map(recipe => {
                                         return <option key={recipe.id} value={recipe.name}>{recipe.name}</option>;
                                     })}
